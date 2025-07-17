9b02dd05eeba6993a7e2b056c338afcb92535f63
Apoorva-Saxena-hub <saxenapoorva1307@gmail.com>
2025-07-17 17:08:09 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const port = process.env.PORT|| 3000;
+const CategoryRoute = require("./routes/category");
+
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGOURL).then(() => console.log("foodly connected to database")).catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use("/api/category",CategoryRoute);
+
+
+app.listen(process.env.PORT || 6013, () => console.log(`Foodly backend is running on ${process.env.PORT}`)) 
